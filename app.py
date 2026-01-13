4ae615eab7ca132da99446049d81ab55f027fc78
Zarhan <ironmanred118@gmail.com>
2026-01-13 14:13:35 +0530

    Prepare app for deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from recommender import recommend_products
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -25,4 +26,6 @@ def recommend():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
